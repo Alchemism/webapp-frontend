9d20c8f6ac3a81dfbadc3296b214323369d67657
Sonia Mahankali <mahankali.s@husky.neu.edu>
2020-02-08 02:34:35 -0500

    random books with next button

---
+++ src/App.js
@@ -0,0 +1,86 @@
+import React, { Component } from "react";
+import "./App.css";
+import getBooks from "./services/BookService";
+import Book from "./components/Book";
+import axios from "axios";
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      books : [],
+      random : ''
+    }
+    axios
+    .get(
+      "http://localhost:8080/book"
+  )
+  .then(response =>
+    {
+    var randval =  response.data[Math.floor(Math.random() * response.data.length)];
+  //console.log(randval);
+    this.setState({books: response.data, random: randval})
+  })
+  }
+
+  chooseRandom = () => {
+    var randval =  this.state.books[Math.floor(Math.random() * this.state.books.length)];
+    //console.log(randval);
+    this.setState({books: this.state.books, random: randval})
+  }
+  render() {
+    //console.log(this.state.books)
+    //this.setState({books: getBooks})
+    return(
+      <div>
+      <table>
+      <thead>
+        <tr>
+          <th>
+            ID
+          </th>
+          <th>
+            Title
+          </th>
+          <th>
+            Author
+          </th>
+          <th>
+          Quantity
+        </th>
+        <th>
+        ISBN
+      </th>
+        </tr>
+      </thead>
+      <tbody>
+  
+      {/*this.state.books.map((book, key) => <Book book={book} key={key}/>)*/} 
+  
+      <tr>
+      <td>
+      {this.state.random.id}
+      </td>
+      <td>
+      {this.state.random.title}
+      </td>
+      <td>
+      {this.state.random.author}
+      </td>
+      <td>
+      {this.state.random.quantity}
+      </td>
+      <td>
+      {this.state.random.isbn}
+      </td>
+      </tr>
+      </tbody>
+    </table>
+    <button onClick={() => this.chooseRandom()}>
+    Next
+    </button>
+    </div>
+    )   
+  }
+}
+
+export default App;
