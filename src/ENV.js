02f0543e3ec263782314052b97b0c7523f435de0
Sonia Mahankali <mahankali.s@husky.neu.edu>
2020-03-13 13:50:42 -0400

    url

--- src/ENV.js
+++ src/ENV.js
@@ -1,4 +1,4 @@
-const url = "localhost";
+const url = "http://a8a69f9d76d77475da8baccabb91e7ba-359624516.us-east-1.elb.amazonaws.com:8080/book";
 
 /*const USERNAME = "sonia@neu.com";
 const PASSWORD = "123456789";
